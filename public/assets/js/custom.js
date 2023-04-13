957ce512df6b42308d188e45bd90f3f05b341a96
baligatra <baligatrasurvey@gmail.com>
2023-04-13 12:34:45 +0800

    Import scripts from bg & some adjustment

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,5 +1,5 @@
 /**	
-	* Template Name: Outing
+	* Template Name: Outing & Logis
 	* Version: 1.0	
 	* Template Scripts
 	* Author: MarkUps
@@ -105,6 +105,72 @@
 		  ]
 		});
 
+	/**
+   * Sticky navbar on scroll
+   */
+	const selectHeaderNavbar = document.querySelector('#headerNavbar');
+	if (selectHeaderNavbar) {
+	  document.addEventListener('scroll', () => {
+		window.scrollY > 100 ? selectHeaderNavbar.classList.add('sticked') : selectHeaderNavbar.classList.remove('sticked');
+	  });
+	}
+
+	  /**
+   * Mobile nav toggle
+   */
+	  const mobileNavShow = document.querySelector('.mobile-nav-show');
+	  const mobileNavHide = document.querySelector('.mobile-nav-hide');
+	
+	  document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+		el.addEventListener('click', function(event) {
+		  event.preventDefault();
+		  mobileNavToogle();
+		})
+	  });
+	
+	  function mobileNavToogle() {
+		document.querySelector('body').classList.toggle('mobile-nav-active');
+		mobileNavShow.classList.toggle('d-none');
+		mobileNavHide.classList.toggle('d-none');
+	  }
+	
+	  /**
+	   * Hide mobile nav on same-page/hash links
+	   */
+	  document.querySelectorAll('#navbar a').forEach(navbarlink => {
+	
+		if (!navbarlink.hash) return;
+	
+		let section = document.querySelector(navbarlink.hash);
+		if (!section) return;
+	
+		navbarlink.addEventListener('click', () => {
+		  if (document.querySelector('.mobile-nav-active')) {
+			mobileNavToogle();
+		  }
+		});
+	
+	  });
+	
+	  /**
+	   * Toggle mobile nav dropdowns
+	   */
+	  const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+	
+	  navDropdowns.forEach(el => {
+		el.addEventListener('click', function(event) {
+		  if (document.querySelector('.mobile-nav-active')) {
+			event.preventDefault();
+			this.classList.toggle('active');
+			this.nextElementSibling.classList.toggle('dropdown-active');
+	
+			let dropDownIndicator = this.querySelector('.dropdown-indicator');
+			dropDownIndicator.classList.toggle('bi-chevron-up');
+			dropDownIndicator.classList.toggle('bi-chevron-down');
+		  }
+		})
+	  });
+	
 	
 	
 })( jQuery );
