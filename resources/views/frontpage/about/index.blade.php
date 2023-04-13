576be9dfd47f868c52b66d9be2520318220fa12a
baligatra <baligatrasurvey@gmail.com>
2023-04-13 12:34:06 +0800

    Add about

---
+++ resources/views/frontpage/about/index.blade.php
@@ -0,0 +1,194 @@
+@extends('layouts.frontapp')
+
+@section('content')
+    <!-- Start About -->
+    <section id="mu-about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mu-about-area">
+                        <!-- Start Feature Content -->
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="mu-about-left">
+                                    <img class="" src="{{ asset('assets/images/about-us-img.jpeg') }}" alt="img">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="mu-about-right">
+                                    <h2>About Our Agency</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam aliquam
+                                        distinctio magni enim error commodi suscipit nobis alias nulla, itaque ex,
+                                        vitae repellat amet neque est voluptatem iure maxime eius!</p>
+
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus in
+                                        accusamus qui sequi nisi, sint magni, ipsam, porro nesciunt id veritatis
+                                        quaerat ipsum consequatur laborum, provident veniam quibusdam placeat quam?
+                                    </p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis corporis
+                                        consectetur quisquam culpa nihil sit vitae, mollitia illum, alias neque veniam
+                                        repellendus magnam error iste quos non, harum enim.</p>
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos alias quod cumque ad
+                                        labore praesentium, voluptatum nesciunt provident quam sapiente dignissimos. Quo ad
+                                        ullam ducimus quam facere sit necessitatibus eius odit rem maiores voluptate libero,
+                                        quisquam dolorum doloribus nulla labore fuga ea optio accusamus accusantium officiis
+                                        quasi. Est, atque incidunt.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Feature Content -->
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End About -->
+    <section id="main-cont">
+        <div class="container">
+            <h2 class="welcome">ABOUT</h2>
+            <div class="row d-flex justify-content-center">
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 mt30">
+                    <img src="https://sanurcycletours.com/images/content/lined-up-cycle.jpg"
+                        class="img-responsive img-rounded"
+                        style="-webkit-box-shadow: 2px 10px 15px -4px rgba(0,0,0,0.75);-moz-box-shadow: 2px 10px 15px -4px rgba(0,0,0,0.75);box-shadow: 2px 10px 15px -4px rgba(0,0,0,0.75);"><br>
+                    <center>
+                        <p class="mt-4"><b><i>"We hope to see you real soon......."</i></b></p>
+                    </center><br>
+                    <p>Sanur's splendid paved beachfront cycle-pathway stretches some 5 kilometers from the Mertasari area
+                        in Southern Sanur, north to the main beach at Jalan Hang Tuah. This makes for a lovely scenic bike
+                        ride, especially in the cool of the early morning. </p>
+                    <p>The village name Sanur originates from two words, "Saha" and "Nuhur" which rally means the passion to
+                        visit a certain place. Located in the southern part of Bali, Sanur is famed for its beautiful strip
+                        of sandy beaches its' spectacular sunrises which rally marks a new day in the life of the Balinese.
+                    </p>
+                    <p>A laid back atmosphere is what is so unique about the village of Sanur where hospitality is
+                        intertwined with the traditions of Balinese culture and religion. The atmosphere from sunrise till
+                        sunset provides endless opportunities of discovery into the daily lives of the Balinese, where each
+                        corner of the village provides a certain exploration for every visitor.</p>
+                    <p>Lines of shady trees along the roads, a beautiful stretch of beachess for sunbathing and swimming, a
+                        strip of shops and markets and a variety of restaurants and bars for wining and dining, the option
+                        is endless in Sanur.</p>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 mt30">
+                    <p style="font-size:20px"><b><i>"​Sanur Cycle Tours is a locally owned and operated business based in
+                                central Sanur and we would like to show you the real Bali. Join us as we take you on a
+                                leisurely ride thru the village of Sanur introducing you to village locals and the Balinese
+                                culture."</i></b></p><br><br><br>
+                    <iframe
+                        src="https://static.parastorage.com/services/santa/1.836.3/static/external/googleMap.html?address=Sanur%20Beach&amp;addressInfo=7klm%20Sanur%20Beach%20Path&amp;language=en&amp;lat=-8.7069803&amp;long=115.26253070000007&amp;mapInteractive=true&amp;mapType=HYBRID&amp;showMapType=true&amp;showPosition=true&amp;showStreetView=false&amp;showZoom=true&amp;ts=990"
+                        width="100%" height="380" frameborder="0" scrolling="no"
+                        data-reactid=".0.$SITE_ROOT.$desktop_siteRoot.$PAGES_CONTAINER.1.1.$SITE_PAGES.$citr.1.$GglMp0.2.0"></iframe><br><br>
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <div id="TA_excellent136" class="TA_excellent">
+                            <ul id="dBlKbeyin" class="TA_links TeH3ZNSrV">
+                                <li id="LT8U6gR" class="iOT5K2cumSs">
+                                    <a target="_blank" href="https://www.tripadvisor.com/"><img
+                                            src="https://static.tacdn.com/img2/widget/tripadvisor_logo_115x18.gif"
+                                            alt="TripAdvisor" class="widEXCIMG" id="CDSWIDEXCLOGO" /></a>
+                                </li>
+                            </ul>
+                        </div>
+                        <script
+                            src="https://www.jscache.com/wejs?wtype=excellent&amp;uniq=136&amp;locationId=3949024&amp;lang=en_US&amp;display_version=2">
+                        </script>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4"><a href="index.php?page=booking" target="_blank"><img
+                                src="https://sanurcycletours.com/images/content/book-tour.gif" class="img-responsive" /></a></div>
+                </div>
+                <br class="clear"><br><br>
+            </div>
+        </div>
+    </section>
+
+    <!-- Start Why Us -->
+    <section id="mu-why-us">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mu-why-us-area">
+                        <h2>Why Us?</h2>
+                        <div class="mu-why-us-content">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="mu-why-us-single">
+                                        <div class="my-why-us-single-icon">
+                                            <i class="fa fa-bed" aria-hidden="true"></i>
+                                        </div>
+                                        <h3>Luxurious Hotels</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae eum
+                                            corporis commodi, ipsum sequi quae nemo quasi voluptatibus quaerat
+                                            nulla! Doloribus cumque ipsum, tempore veritatis quibusdam quae numquam
+                                            minus iste!</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="mu-why-us-single">
+                                        <div class="my-why-us-single-icon">
+                                            <i class="fa fa-thumbs-up" aria-hidden="true"></i>
+                                        </div>
+                                        <h3>The Best Service</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae eum
+                                            corporis commodi, ipsum sequi quae nemo quasi voluptatibus quaerat
+                                            nulla! Doloribus cumque ipsum, tempore veritatis quibusdam quae numquam
+                                            minus iste!</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="mu-why-us-single">
+                                        <div class="my-why-us-single-icon">
+                                            <i class="fa fa-plane" aria-hidden="true"></i>
+                                        </div>
+                                        <h3>Unique Experience</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae eum
+                                            corporis commodi, ipsum sequi quae nemo quasi voluptatibus quaerat
+                                            nulla! Doloribus cumque ipsum, tempore veritatis quibusdam quae numquam
+                                            minus iste!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Why Us -->
+
+    <!-- Start Video -->
+    <section id="mu-video">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mu-video-area">
+                        <h2>Watch Our Recent Trip Video</h2>
+                        <p class="mu-title-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
+                            impedit nostrum porro possimus nesciunt voluptate, id nam. Harum deserunt nobis esse
+                            iste voluptatem cumque neque quo sunt velit. Nostrum, provident!</p>
+
+                        <!-- Start Video content -->
+                        <div class="mu-video-content">
+                            <iframe width="854" height="480" src="https://www.youtube.com/embed/-DQo0Ccacy8"
+                                allowfullscreen></iframe>
+                        </div>
+                        <!-- End Video content -->
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Video -->
+
+    <!-- Start Google Map -->
+
+    <div id="mu-google-map">
+        <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3508.8176744277202!2d-81.47150788457147!3d28.424757900613237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e77e378ec5a9a9%3A0x2feec9271ed22c5b!2sOrange+County+Convention+Center!5e0!3m2!1sen!2sbd!4v1503833952781" width="850" height="450" allowfullscreen></iframe> -->
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2788.9849115472457!2d115.23315677083276!3d-8.670589179280855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd240f51f1412a5%3A0xa7e549b0b86dc50e!2sLapangan%20Puputan%20Renon!5e0!3m2!1sid!2sid!4v1681191178920!5m2!1sid!2sid"
+            width="850" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+
+    <!-- End Google Map -->
+@endsection
