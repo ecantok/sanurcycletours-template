4ee69ca051bb62e89751e3b59373acf29a532cf2
Hans-Star18 <agussuardiasa1231@gmail.com>
2023-05-22 17:23:28 +0800

    add new image

--- resources/views/layouts/frontpage/metatags.blade.php
+++ resources/views/layouts/frontpage/metatags.blade.php
@@ -24,27 +24,31 @@
 
     <!-- Meta Tags Generated via http://heymeta.com -->
 @else
-    <meta name="description" content="Sanur cycle tours">
-    <meta name="keywords" content="">
-    <meta name="author" content="https://sanurcycletours.com">
-    <meta name="distribution" content="global" />
+    <!-- HTML Meta Tags -->
+    <title>Sanur Cycle Tours - Guided Tours of Sanur Bali</title>
+    <meta name="description"
+        content="Sanur Cycle Tours is a100% locally owned and operated business based in central Sanur and we would like to show you the real Sanur village & Bali with our 3 to 4 hour local tours as well as our longer & multi-day adventure cycling Tours. Join us as we take you on a leisurely ride thru the village of Sanur introducing you to village locals and the Balinese culture. Or allow us to share the real Bali & Nusa Penida Island with you on our specialised Adventure rides!">
+
     <!-- Google / Search Engine Tags -->
-    <meta itemprop="name" content="Sanur Cycle Tours">
-    <meta itemprop="description" content="Sanur cycle tours">
-    <meta itemprop="image" content="{{ asset('assets/images/cycle-logo.jpg') }}">
+    <meta itemprop="name" content="Sanur Cycle Tours - Guided Tours of Sanur Bali">
+    <meta itemprop="description"
+        content="Sanur Cycle Tours is a100% locally owned and operated business based in central Sanur and we would like to show you the real Sanur village & Bali with our 3 to 4 hour local tours as well as our longer & multi-day adventure cycling Tours. Join us as we take you on a leisurely ride thru the village of Sanur introducing you to village locals and the Balinese culture. Or allow us to share the real Bali & Nusa Penida Island with you on our specialised Adventure rides!">
+    <meta itemprop="image" content="{{ asset('assets/images/favicon.ico') }}">
 
     <!-- Facebook Meta Tags -->
     <meta property="og:url" content="https://sanurcycletours.com">
     <meta property="og:type" content="website">
-    <meta property="og:title" content="Sanur Cycle Tours">
-    <meta property="og:description" content="Sanur cycle tours">
-    <meta property="og:image" content="{{ asset('assets/images/cycle-logo.jpg') }}">
+    <meta property="og:title" content="Sanur Cycle Tours - Guided Tours of Sanur Bali">
+    <meta property="og:description"
+        content="Sanur Cycle Tours is a100% locally owned and operated business based in central Sanur and we would like to show you the real Sanur village & Bali with our 3 to 4 hour local tours as well as our longer & multi-day adventure cycling Tours. Join us as we take you on a leisurely ride thru the village of Sanur introducing you to village locals and the Balinese culture. Or allow us to share the real Bali & Nusa Penida Island with you on our specialised Adventure rides!">
+    <meta property="og:image" content="{{ asset('assets/images/favicon.ico') }}">
 
     <!-- Twitter Meta Tags -->
     <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="Sanur Cycle Tours">
-    <meta name="twitter:description" content="Sanur cycle tours">
-    <meta name="twitter:image" content="{{ asset('assets/images/cycle-logo.jpg') }}">
+    <meta name="twitter:title" content="Sanur Cycle Tours - Guided Tours of Sanur Bali">
+    <meta name="twitter:description"
+        content="Sanur Cycle Tours is a100% locally owned and operated business based in central Sanur and we would like to show you the real Sanur village & Bali with our 3 to 4 hour local tours as well as our longer & multi-day adventure cycling Tours. Join us as we take you on a leisurely ride thru the village of Sanur introducing you to village locals and the Balinese culture. Or allow us to share the real Bali & Nusa Penida Island with you on our specialised Adventure rides!">
+    <meta name="twitter:image" content="{{ asset('assets/images/favicon.ico') }}">
 
     <!-- Meta Tags Generated via http://heymeta.com -->
 @endif
